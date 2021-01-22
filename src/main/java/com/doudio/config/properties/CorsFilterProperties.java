f826e26bcd6a5d304feaf0536b44c58b68354676
qwl <2491920818@qq.com>
2021-01-22 16:59:04 +0800

    添加备注点击劫持禁止

--- src/main/java/com/doudio/config/properties/CorsFilterProperties.java
+++ src/main/java/com/doudio/config/properties/CorsFilterProperties.java
@@ -10,6 +10,22 @@ import java.util.Map;
  * @author: doudio
  * @date: 2021-01-14 11:54
  * @description: 跨域过滤器配置
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+ *
+ * https://developer.mozilla.org/zh-cn/docs/Web/HTTP/Headers/Content-Security-Policy
+ *
+ * 一、点击劫持
+ * X-FRAME-OPTIONS
+ *
+ * X-FRAME-OPTIONS是一个 HTTP 响应头，在现代浏览器有一个很好的支持。这个 HTTP 响应头 就是为了防御用 iframe 嵌套的点击劫持攻击。
+ *
+ * 该响应头有三个值可选，分别是
+ *
+ * DENY，表示页面不允许通过 iframe 的方式展示
+ * SAMEORIGIN，表示页面可以在相同域名下通过 iframe 的方式展示
+ * ALLOW-FROM，表示页面可以在指定来源的 iframe 中展示
+ *
  **/
 @Data
 @ConfigurationProperties("doudio.filter.cors")
